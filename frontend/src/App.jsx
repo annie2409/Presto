f1e08522debc2e01d196170e84428a56c9eb42c5
Annie <03.huynhphantramanh.9at2@gmail.com>
2024-04-01 14:35:11 +1100

    Add navbar and login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,36 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import Routes and Route components
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
+import { Home } from './pages/Home';
+import { ErrorBoundary } from 'react-error-boundary';
+
+const fallbackRender = ({ error, resetErrorBoundary }) => {
+  // Call resetErrorBoundary() to reset the error boundary and retry the render.
+
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: 'red' }}>{error.message}</pre>
+    </div>
+  );
+}
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary fallbackRender={fallbackRender}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 };
+
+export const navBarPages = [
+  { to: '/home', text: 'Home' }
+]
 
 export default App;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,63 @@
 import React from 'react';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 export const Login = () => {
-  return <div>Login page</div>;
+  return (
+    <div>
+      <form>
+        <Box
+          display='flex'
+          flexDirection='column'
+          minWidth={300}
+          width='45%'
+          maxWidth={450}
+          alignItems='center'
+          justifyContent='center'
+          margin='auto'
+          marginTop={5}
+          padding={5}
+          borderRadius={5}
+          boxShadow={'5px 5px 10px #ccc'}
+          sx={{
+            ':hover': {
+              boxShadow: '10px 10px 20px, #ccc'
+            }
+          }}
+        >
+          <Typography variant='h2' padding={3} textAlign='center'>Login</Typography>
+          <TextField
+            variant='outlined'
+            placeholder='Email'
+            type='email'
+            margin='normal'
+            sx={{ width: '70%' }}
+          />
+          <TextField
+            variant='outlined'
+            placeholder='Password'
+            type='password'
+            margin='normal'
+            sx={{ width: '70%' }}
+          />
+          <Button
+            variant='contained'
+            color='primary'
+            sx={{
+              marginTop: 3
+            }} >
+            Login
+          </Button>
+          <Link to="/register">
+            <Button
+              sx={{
+                marginTop: 3
+              }} >
+              Sign up
+            </Button>
+          </Link>
+        </Box>
+      </form>
+    </div>
+  );
 };
